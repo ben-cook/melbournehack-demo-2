7a1ad0ceada9ddeedf77608487804403b9bb6aca
Ben Cook <mcbeanoo@gmail.com>
2021-08-14 13:16:59 +1000

    3-add-basic-ui

--- src/App.js
+++ src/App.js
@@ -1,4 +1,11 @@
-import { Container, Typography } from "@material-ui/core";
+import {
+  Container,
+  Typography,
+  Card,
+  Grid,
+  CardContent,
+  CardMedia,
+} from "@material-ui/core";
 import axios from "axios";
 import { useEffect, useState } from "react";
 
@@ -9,12 +16,30 @@ function App() {
   const [data, setData] = useState(null);
 
   useEffect(() => {
-    axios(API_URL).then((res) => setData(res.data));
+    axios(API_URL).then((res) => setData(res.data.slice(0, 15)));
   }, []);
 
   return (
     <Container>
       <Typography variant="h3">Cryptocurrencies!</Typography>
+
+      <Grid container spacing={2}>
+        {data &&
+          data.map((coin) => (
+            <Grid item key={coin.name} xs={3}>
+              <Card>
+                <CardMedia
+                  image={coin.image}
+                  style={{ height: 250 }}
+                ></CardMedia>
+                <CardContent>
+                  <Typography variant="h6">{coin.name}</Typography>
+                </CardContent>
+              </Card>
+            </Grid>
+          ))}
+      </Grid>
+
       <pre>{JSON.stringify(data, undefined, 2)}</pre>
     </Container>
   );
